b50b7989a9e05c0a9711b094238ea91a6beca1b0
Yash Naik <yashnaik47@gmail.com>
2019-07-29 21:38:09 +0530

    Updates

---
+++ precache-manifest.7e4ef459d35066a4e99357807dfdd1e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0949d3d34d50a8744a46c8a787ffff2",
+    "url": "/router-demo/index.html"
+  },
+  {
+    "revision": "00743d63461f9b226a33",
+    "url": "/router-demo/static/js/2.b8dff7f6.chunk.js"
+  },
+  {
+    "revision": "2ac4df92b9398f21268e",
+    "url": "/router-demo/static/js/main.3d6fc3ad.chunk.js"
+  },
+  {
+    "revision": "eb2f93f6c752525ef90e",
+    "url": "/router-demo/static/js/runtime~main.36dec3c3.js"
+  }
+]);
